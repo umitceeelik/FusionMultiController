4e982301e646156300e16e671d117f5aed86c7d4
umitceeelik <umitclk2001@gmail.com>
2023-01-04 20:44:25 +0300

    feat: Added Multiplayer networking with animation using photon fusion

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using System;
+using System.ComponentModel;
+
+public class Player : NetworkBehaviour
+{
+    private NetworkCharacterControllerPrototype _cc;
+
+    [Header("Animation")]
+    public Animator characterAnimator;
+
+    float walkSpeed = 0;
+
+    private void Awake()
+    {
+        _cc = GetComponent<NetworkCharacterControllerPrototype>();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.direction.Normalize();
+            _cc.Move(5 * data.direction * Runner.DeltaTime);
+
+            Vector2 walkVector = new Vector2(_cc.Velocity.x, _cc.Velocity.z);
+            walkVector.Normalize();
+
+            walkSpeed = Mathf.Lerp(walkSpeed, Mathf.Clamp01(walkVector.magnitude), Runner.DeltaTime * 5);
+
+            characterAnimator.SetFloat("MovementSpeed", walkSpeed);
+        }
+    }
+}
+
